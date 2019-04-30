0b54796818efc6b9fbb1c6df294e513d198cfd8e
Dan <dansonlinepresence@gmail.com>
2019-04-30 01:46:11 -0400

    POST semantics

--- __main__.py
+++ __main__.py
@@ -1,9 +1,12 @@
 import argparse
+from datetime import datetime
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import inspect
 import json
 import os
+from pprint import pprint
 import sys
+import uuid
 
 parser = argparse.ArgumentParser()
 parser.add_argument('module')
@@ -14,40 +17,70 @@ if args.module.endswith('.py'): args.module = args.module[:-3]
 sys.path.append(os.path.dirname(args.module))
 exec('import {} as module'.format(os.path.basename(args.module)))
 
-def parse_requestline(r): return r.split()[1].split('/')[1:]
+store = {}
 
 class Handler(BaseHTTPRequestHandler):
-    def send_json_response(self, j):
+    def print_request_start(self):
+        print('===== {} ====='.format(self.requestline))
+
+    def parse(self):
+        url_path = self.requestline.split()[1].split('/')[1:]
+        command, attr_path = url_path[0], url_path[1:]
+        attr = module
+        for i in attr_path:
+            if i: attr = getattr(attr, i)
+        return (command, attr)
+
+    def send_json_response(self, json_content=None):
         self.send_response(200)
-        self.send_header('Content-Type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(j).encode('utf-8'))
+        if json_content is not None:
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header('Content-Type', 'application/javascript')
+            self.end_headers()
+            pprint(json_content)
+            self.wfile.write(json.dumps(json_content).encode('utf-8'))
 
     def do_GET(self):
-        path = parse_requestline(self.requestline)
-        print('path: {}, requestline: {}'.format(path, self.requestline))
-        x = module
-        for i in path:
-            if i: x = getattr(x, i)
-        r = {
-            'value': repr(x),
-            'type': repr(type(x)),
-            'dir': dir(x),
-        }
-        try:
-            r['args'] = repr(inspect.getargspec(x))
-        except: pass
-        self.send_json_response(r)
+        self.print_request_start()
+        command, attr = self.parse()
+        if command == 'inspect':
+            json_content = {
+                'value': repr(attr),
+                'dir': dir(attr),
+            }
+            try: json_content['args'] = repr(inspect.getargspec(attr))
+            except: pass
+        self.send_json_response(json_content)
 
-'''
-post /expression
-    eval(expression)(a1=a1, a2=a2...)
-    returns id that can be referenced
-
-post /id/expression
-    get result corresponding to expression/id
-    returns 
-'''
+    def do_POST(self):
+        self.print_request_start()
+        def parse_body():
+            length = int(self.headers.get('Content-Length'))
+            if length:
+                kwargs = json.loads(self.rfile.read(length))
+            else:
+                kwargs = {}
+            for k, v in kwargs.items():
+                if v in store:
+                    kwargs[k] = store[v]
+            pprint(kwargs)
+            return kwargs
+        command, attr = self.parse()
+        if command == 'store':
+            id = str(uuid.uuid4())
+            store[id] = attr(**parse_body())
+            json_content = id
+        elif command == 'get':
+            json_content = attr(**parse_body())
+        elif command == 'store-and-get':
+            id = str(uuid.uuid4())
+            value = attr(**parse_body())
+            store[id] = value
+            json_content = {'id': id, 'value': value}
+        elif command == 'unstore':
+            for i in json.loads(self.rfile.read()): del store[i]
+            json_content = None
+        self.send_json_response(json_content)
 
 server_address = ('', int(args.port))
 HTTPServer(server_address, Handler).serve_forever()
