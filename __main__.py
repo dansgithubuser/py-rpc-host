22e2c16b4b4e32c3a250ef02af9d21d28c02a148
Dan <dansonlinepresence@gmail.com>
2019-04-30 13:29:57 -0400

    add eval, pdb; remove store-and-get; separate attr name with . instead of /; command -> op; fix handling of op without attr name
    makes no real sense to separate an attr name with / just because it's in a URL
    makes it easier to separate other parameters by using anything else though
    
    op is shorter than command,
    and python http server seems to call http method command

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import inspect
 import json
 import os
+import pdb
 from pprint import pprint
 import sys
 import uuid
@@ -24,32 +25,39 @@ class Handler(BaseHTTPRequestHandler):
         print('===== {} ====='.format(self.requestline))
 
     def parse(self):
-        url_path = self.requestline.split()[1].split('/')[1:]
-        command, attr_path = url_path[0], url_path[1:]
+        split = self.requestline.split()[1].split('/')[1:]
+        op = split[0]
+        if len(split) > 1:
+            attr_name = split[1]
+        else:
+            attr_name = ''
         attr = module
-        for i in attr_path:
+        for i in attr_name.split('.'):
             if i: attr = getattr(attr, i)
-        return (command, attr)
+        return (op, attr)
 
     def send_json_response(self, json_content=None):
+        if json_content is None: json_content = {}
         self.send_response(200)
-        if json_content is not None:
-            self.send_header('Access-Control-Allow-Origin', '*')
-            self.send_header('Content-Type', 'application/javascript')
-            self.end_headers()
-            pprint(json_content)
-            self.wfile.write(json.dumps(json_content).encode('utf-8'))
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Content-Type', 'application/javascript')
+        self.end_headers()
+        pprint(json_content)
+        self.wfile.write(json.dumps(json_content).encode('utf-8'))
 
     def do_GET(self):
         self.print_request_start()
-        command, attr = self.parse()
-        if command == 'inspect':
+        op, attr = self.parse()
+        if op == 'inspect':
             json_content = {
                 'value': repr(attr),
                 'dir': dir(attr),
             }
             try: json_content['args'] = repr(inspect.getargspec(attr))
             except: pass
+        elif op == 'pdb':
+            pdb.set_trace()
+            json_content = {}
         self.send_json_response(json_content)
 
     def do_POST(self):
@@ -65,19 +73,17 @@ class Handler(BaseHTTPRequestHandler):
                     kwargs[k] = store[v]
             pprint(kwargs)
             return kwargs
-        command, attr = self.parse()
-        if command == 'store':
+        op, attr = self.parse()
+        if op == 'store':
             id = str(uuid.uuid4())
             store[id] = attr(**parse_body())
             json_content = id
-        elif command == 'get':
+        elif op == 'eval':
+            attr(**parse_body())
+            json_content = None
+        elif op == 'get':
             json_content = attr(**parse_body())
-        elif command == 'store-and-get':
-            id = str(uuid.uuid4())
-            value = attr(**parse_body())
-            store[id] = value
-            json_content = {'id': id, 'value': value}
-        elif command == 'unstore':
+        elif op == 'unstore':
             for i in json.loads(self.rfile.read()): del store[i]
             json_content = None
         self.send_json_response(json_content)
